8a9212bffaf80ccff4a64dc99c3cf362bc1ac1d9
SRVONDINA\Elena <elrome.tronel@gmail.com>
2021-11-27 11:01:41 +0100

    A ver si esta vez funciona con los nombres de las carpetas renombrados

---
+++ src/components/Current-data/index.jsx
@@ -0,0 +1,105 @@
+import {  Typography, Stack, CardMedia } from '@mui/material';
+import './style.css'
+import faseLunar from './faseLunar/Asset 21.png';
+
+
+export default function ActualInfo(){
+
+
+    return (
+        
+        
+        <Stack xs={12} 
+            direction="row" 
+            spacing={5}
+            justifyContent="space-around"
+            alignItems="center"
+            square={true} className="card_ActualInfo" >
+            <Stack direction="column"
+                justifyContent="center"
+                alignItems="center"
+                width="4em"
+                spacing={-3}>
+                <Typography gutterBottom variant="h6" component="h4">
+                    34º C
+                </Typography>        
+                <Typography variant="body2" color="textSecondary" component="p"textAlign="center">
+                    Sensación térmica
+                </Typography>        
+               
+            </Stack>
+             <Stack direction="column"
+                justifyContent="center"
+                alignItems="center"
+                width="4em"
+                spacing={-3}>
+                <Typography gutterBottom variant="h6" component="h4">
+                    5 KPH
+                </Typography>        
+                <Typography variant="body2" color="textSecondary" component="p" textAlign="center">
+                    Velocidad viento
+                    </Typography>       
+               
+            </Stack>
+             <Stack direction="column"
+                justifyContent="center"
+                alignItems="center"
+                width="4em"
+                spacing={-3}>
+                <Typography gutterBottom variant="h6" component="h4">
+                    60%
+                </Typography>        
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Humedad ambiental
+                </Typography>        
+            </Stack>
+            <Stack direction="column"
+                justifyContent="center"
+                alignItems="center"
+                width="4em"
+                spacing={-3}>
+                <Typography gutterBottom variant="h6" component="h4">
+                    Alto
+                </Typography>        
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Índice UV
+                </Typography>        
+            </Stack>
+     
+           <Stack direction="column"
+                justifyContent="center"
+                alignItems="center"
+                width="4em"
+                spacing={-3}>
+                <Typography gutterBottom variant="h6" component="h4">
+                    8:12
+                </Typography>        
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Amanecer
+                </Typography>
+                <Typography gutterBottom variant="h6" component="h4">
+                    18:12
+                </Typography>        
+                <Typography variant="body2" color="textSecondary" component="p">
+                    Atardecer
+                </Typography>
+            </Stack>
+            <Stack direction="column"
+                justifyContent="center"
+                alignItems="center"
+                >
+                    <CardMedia
+                        component="img"
+                        className= "image_actualinfo"
+                        alt="icono tiempo diario"
+                        image={faseLunar}
+
+                    />
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        Fase Lunar
+                    </Typography>
+            </Stack>
+        </Stack>
+
+    );
+}
---
+++ src/components/Week-weather/index.jsx
@@ -0,0 +1,25 @@
+import { Grid } from '@mui/material';
+import WeekCard from './cardWeekDay';
+import './style.css';
+
+
+export default function WeekDayContainer(){
+
+
+    return(
+        
+        <Grid container direction="row">
+            <Grid className="grid_container_card" item container direction="row" xs={12}>
+
+            <WeekCard></WeekCard>
+            <WeekCard></WeekCard>
+            <WeekCard></WeekCard>
+            <WeekCard></WeekCard>
+            <WeekCard></WeekCard>
+            <WeekCard></WeekCard>
+            <WeekCard></WeekCard>
+            
+            </Grid>
+        </Grid>
+    )
+}
