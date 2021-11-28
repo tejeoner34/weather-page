000b68775fe39f1f612823b98be652df1e851a9a
tejeoner34 <atzarco37@gmail.com>
2021-11-28 19:47:05 +0100

    background image pasada por props.

--- src/components/actualDay/index.jsx
+++ src/components/actualDay/index.jsx
@@ -3,29 +3,27 @@ import ActualInfo from "../actualInfo";
 import DailiesComponent from '../dailiesComponent';
 import AvatarContainer from '../../components/avatarContainer';
 import './style.css'
-import Niebla from '../../page/assets/bgImagesDiarias/lluviamedio.png';
 
-const styles = {
-    gridContainer: {
-        backgroundImage: `url(${Niebla})`
-    }
-};
+
 
 export default function ActualDay(props) {
 
     return (
-        <Grid container className="grid__style--item" style={styles.gridContainer}>
-            <Grid item container xs={12} 
-            justifyContent="space-between" 
-            spacing={2}>
+        <Grid container className="grid__style--item" sx={{backgroundImage: `url(${window.location.origin + `/img/weather-img/${props.data?.current?.weather[0].icon}.png`})`}}>
+            <Grid item container xs={12}
+                justifyContent="space-between"
+                spacing={2}>
                 <Grid item xs={3}>
                     <Box sx={{
                         border: 'solid',
                     }}>contenido</Box>
                 </Grid>
-                
+
                 <Grid item container xs={2} justifyContent="center" alignItems='center' flexDirection='column' rowGap={2} >
-                    <Grid item container justifyContent="space-between" alignItems='space-between' flexDirection='column' rowGap={3}><DailiesComponent data={props.data}></DailiesComponent></Grid>
+                    <Grid item container justifyContent="space-between" alignItems='space-between' flexDirection='column' rowGap={3}>
+                        <DailiesComponent data={props.data}></DailiesComponent>
+                    </Grid>
+
                     <Grid item><AvatarContainer data={props.current}></AvatarContainer></Grid>
                 </Grid>
                 <Grid item xs={12}>
