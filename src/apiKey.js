8c65d40cbc4d7d872205e07d276422e4a81ffdca
manu-grande-lope <manugrande@gmail.com>
2021-11-29 18:57:31 +0100

    sombras,tipos,estilos hechos

---
+++ src/apiKey.js
@@ -0,0 +1,2 @@
+
+export const apiKey = "fbe6cfd7582195b7a5d395286d5ea052";
--- src/components/weekDayContainer/index.jsx
+++ src/components/weekDayContainer/index.jsx
@@ -9,7 +9,7 @@ export default function WeekDayContainer(props){
 
     return(
         <Grid container direction="row">
-            <Grid item container direction="row" xs={12} justifyContent="space-evenly">
+            <Grid item container direction="row" xs={12} justifyContent="flex-start">
                 {props.data.daily?.slice(1,8).map((d,index)=> <WeekCard  key={index} data={d} ></WeekCard>)}
             
             </Grid>
